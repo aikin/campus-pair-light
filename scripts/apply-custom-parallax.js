952d9ffa5a59af6e69cf4e93c03a9043f8bf9d2a
aikin <me@ulaijn.com>
2016-05-15 23:08:13 +0800

    aikin: fix path

--- scripts/apply-custom-parallax.js
+++ scripts/apply-custom-parallax.js
@@ -1,13 +1,13 @@
 Parallax([
     {
         section: '.introSection',
-        pictureUrl: '../images/sign-up.png',
+        pictureUrl: 'images/sign-up.png',
         pictureWidth: 1600,
         pictureHeight: 1199
     },
     {
         section: '.companyIntroSection',
-        pictureUrl: '../images/company-thoughtworks.png',
+        pictureUrl: 'images/company-thoughtworks.png',
         pictureWidth: 3333,
         pictureHeight: 2292
     }
