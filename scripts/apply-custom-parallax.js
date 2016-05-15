d16386d2d845d2b963db530b2be52bbcc424e01c
Fox Team <cwpdevteam@thoughtworks.com>
2016-05-15 23:00:18 +0800

    [taotao] add parallax effect

---
+++ scripts/apply-custom-parallax.js
@@ -0,0 +1,14 @@
+Parallax([
+    {
+        section: '.introSection',
+        pictureUrl: '../images/sign-up.png',
+        pictureWidth: 1600,
+        pictureHeight: 1199
+    },
+    {
+        section: '.companyIntroSection',
+        pictureUrl: '../images/company-thoughtworks.png',
+        pictureWidth: 3333,
+        pictureHeight: 2292
+    }
+], '.bodyWrapper');
---
+++ scripts/custom-parallax.js
@@ -0,0 +1,61 @@
+window.Parallax = function(config, backgroundContainerSelector) {
+    var backgroundContainer = $(backgroundContainerSelector);
+    backgroundContainer.css('background-size', 'contain');
+    backgroundContainer.css('background-repeat', 'no-repeat');
+
+    var watcher = setInterval(function() {
+        if($(_.last(config).section).length !== 0){
+            clearInterval(watcher);
+            parallax();
+        }
+    }, 10);
+    
+    $(window).scroll(parallax);
+
+    function parallax(){
+        var sectionConfigs = _.map(config, readConfig);
+
+        var urls = _.chain(sectionConfigs).map(function(config){
+            return 'url("' + config._pictureUrl + '")';
+        }).join(', ').value();
+
+        backgroundContainer.css('background-image', urls);
+
+
+        var yPositions = _.chain(sectionConfigs).map(function(config){
+            var picturePos = calcPicturePos(config);
+
+            return Math.floor(picturePos) + 'px';
+        }).join(', ').value();
+
+
+        backgroundContainer.css('background-position-y', yPositions);
+    }
+
+    function readConfig(config) {
+        var _sectionSelector = config.section;
+        var _pictureUrl = config.pictureUrl;
+        var _picHeight = config.pictureHeight;
+        var _picWidth = config.pictureWidth;
+
+        var _pictureHeight = (_picHeight / _picWidth) * backgroundContainer.width();
+        return {_sectionSelector: _sectionSelector, _pictureUrl: _pictureUrl, _pictureHeight: _pictureHeight};
+    }
+
+    function calcPicturePos(config) {
+        var section = $(config._sectionSelector);
+        var sectionPos = section.offset().top;
+        var sectionHeight = section.height();
+        var sectionVisibility = calcSectionVisibility(sectionPos, sectionHeight);
+
+        return sectionPos - sectionVisibility * (config._pictureHeight - sectionHeight);
+    }
+
+    function calcSectionVisibility(sectionPos, sectionHeight) {
+        var sectionVanishOffset = -sectionHeight;
+        var sectionOffset = sectionPos - $(window).scrollTop();
+
+        var sectionProgress = (sectionOffset - sectionVanishOffset) / (sectionHeight + $(window).height());
+        return 1 - sectionProgress;
+    }
+};
